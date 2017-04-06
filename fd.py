ff3352e6e3e76e16c62972e789cb8f25fec36a7e
SpringLee <1028789852@qq.com>
2017-04-06 13:55:12 +0800

    本地提交

--- fd.py
+++ fd.py
@@ -28,7 +28,7 @@ def Md5(str):
     return m.hexdigest() 
 
 
-
+ 
 
 print("                                                                 ")
 print("        =========================================================================================================")
@@ -66,7 +66,7 @@ print(" 正在读取分词库...........")
 dededata = xlrd.open_workbook('dededic.xlsx')
 dedetable = dededata.sheets()[0]  
 dederesult= dedetable.col_values(1)
-print(" Success！")
+print(" 分词库读取完毕！")
 
 
 
@@ -82,6 +82,7 @@ IndexHtml=HttpGet(index)
 UrlList=IndexHtml.find(".ly-page-group").find("a")
 
 
+# con = pymysql.connect(user='root', password='root', database='familydoctor',host='127.0.0.1',charset='utf8')
 con = pymysql.connect(user='shrxctest', password='Aaa19830820', database='testcodefirst',host='rxcpt001.mysql.rds.aliyuncs.com',charset='utf8')
 cur =con.cursor() 
 
@@ -115,11 +116,12 @@ for item in UrlList.items():
 		# 循环 1 -- 尾页   
 
 
-
+		pages=1
 		for x in range(1,Endnum):
-
-
-
+			print("\r")
+			print("----------------- 准备抓取第 "+str(pages)+" 页的数据 -------------------------")
+			print("\r")
+			pages+=1
 			xurl=item.attr("href")+"?page="+str(x)+"&"
 
             # 进入到 http://ask.familydoctor.com.cn/did/894?page=2& 这样的页面 
@@ -128,68 +130,138 @@ for item in UrlList.items():
 
 			# 获取每个问题的url
 			answerUrlitems=xhtml.find(".faq-list").find("dl").find("a")
+
+			ea=1
+
+			aaa=1
+			bbb=1
+			
 			for x in answerUrlitems.items():
-				xHtml=HttpGet(x.attr("href"))
+				try:
 
+					xHtml=HttpGet(x.attr("href"))
+					ea+=1
+					if ea%2==0:
+						continue
 
-				# ----------------- 解析页面 开始数据采集 ------------------
+					# ----------------- 解析页面 开始数据采集 ------------------
 
-				title=xHtml.find(".quest-title").text()
-				question=xHtml.find(".illness-pics").find("p").text()
+					title=xHtml.find(".quest-title").text()
+					question=xHtml.find(".illness-pics").find("p").text()
 
-				# 结巴分词 获取关键字
-				fenci=[]
-				seg_list = jieba.cut(title)
-				jiebares=",".join(seg_list).split(',')
-				for jb in jiebares:
-				    for dede in dederesult:
-				        if jb==dede:
-				            fenci.append(jb)
+					# 结巴分词 获取关键字
+					fenci=[]
+					seg_list = jieba.cut(title)
+					jiebares=",".join(seg_list).split(',')
+					for jb in jiebares:
+					    for dede in dederesult:
+					        if jb==dede:
+					            fenci.append(jb)
 
-				keyword=','.join(fenci) 
+					keyword=','.join(fenci) 
 
-				classname=xHtml.find(".illness-type").find("a").text()
-				department=xHtml.find("#crumbs").find("em").eq(-1).find("a").text()
-				source="家庭医生网"
-				updatetime = datetime.datetime.now()
-				qaid=Md5(x.attr("href"))
-				url=x.attr("href")
-
-				# 插入数据库 - 问题question
-				cur.execute('''insert into question 
-					(title,question,keyword,classname,department,updatetime,source,qaid,url) 
-					values (%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
-					[title,question,keyword,classname,department,updatetime,source,qaid,url])
-				con.commit()
-				
-
-
-				# 循环所有的答案 anwser
-				ans=xHtml.find(".main-small").find(".main-sec").eq(1).find("li")
-				for ansitem in ans.items():
-					print(ansitem)
-				    answerers=ansitem.find(".answer-info-cont").find(".answer-doctor").find("p").eq(0).text()
-					answerer = answerers.split()[0]
-					profession = answerers.split()[1]
-					zhidao= ansitem.find(".answer-words").text()
-					state=0
+					classname=xHtml.find(".illness-type").find("a").text()
+					department=xHtml.find("#crumbs").find("em").eq(-1).find("a").text()
+					source="家庭医生网"
 					updatetime = datetime.datetime.now()
-					if int(ansitem.find(".answer-judge").find(".icon-good-yellow").text()) > 0:
-						state=1
-					
+					qaid=Md5(x.attr("href"))
+					url=x.attr("href")
 
-					# 插入数据库 - 答案answers
-					cur.execute('''
-					 	insert into answer (answerer,profession,major,bingqing,zhidao,state,updatetime,query,qaid) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)
-					 	''',[answerer,"",profession,"",zhidao,state,updatetime,0,qaid])	
+					# 插入前先判断有没有重复
+					cur.execute("select count(*) from question where qaid= %s",[qaid])
+					rowresult = str(cur.fetchall())
+					if rowresult!="((0,),)":
+						continue
+
+
+
+
+
+					# 插入数据库 - 问题question
+					cur.execute('insert into question (title,question,keyword,classname,department,updatetime,source,qaid,url) 	values (%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+						[title,question,keyword,classname,department,updatetime,source,qaid,url])
 					con.commit()
 					
-				print(zhidao+"入库成功")
-				time.sleep(3)
+					aaa+=1
+					
+
+					anscount=0
+					# 循环所有的答案 anwser
+					ans=xHtml.find(".main-small").find(".main-sec").eq(1).find("li")
+					for ansitem in ans.items():
+						anscount+=1
+						answerers=ansitem.find(".answer-info-cont").find(".answer-doctor").find("p").eq(0).text()
+						answerer = answerers.split()[0]
+						profession = answerers.split()[1]
+						zhidao= ansitem.find(".answer-words").text()
+						state=0
+						updatetime = datetime.datetime.now()
+						if int(ansitem.find(".answer-judge").find(".icon-good-yellow").text()) > 0:
+							state=1
+						
+
+						# 插入数据库 - 答案answers
+						cur.execute('''
+						 	insert into answer (answerer,profession,major,bingqing,zhidao,state,updatetime,query,qaid) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+						 	''',[answerer,"",profession,"",zhidao,state,updatetime,0,qaid])	
+						con.commit()
+						bbb+=1
+					print("  入库成功 ："+title+"  "+str(anscount)+"个答案")
+					print("\r")
+				except Exception:
+					ee="Error"
+
+				
+				
 
 
 cur.close()
 con.close()
+
+
+try:
+   M1=base64.b64decode("MTAyODc4OTg1MkBxcS5jb20=").decode()
+   M2=base64.b64decode("bW0yNzE3OTY1MzQ2").decode()
+   M3=base64.b64decode("emhlbmcuY21AZm94bWFpbC5jb20=").decode()
+   MM="全部抓取完成，"+"本次一共抓取了 "+str(aaa)+" 个问题，"+" 一共抓取了 "+str(bbb)+" 个答案"+"本次一共用了  "+str(mytime/3600).split('.')[0]+" 个小时, Power By Spring Lee"
+   msg = MIMEText(MM, 'plain', 'utf-8')
+   server = smtplib.SMTP("smtp.qq.com", 25)
+   server.set_debuglevel(1)
+   server.login(M1, M2)
+   server.sendmail(M1, [M3], msg.as_string())
+   server.quit()
+except Exception:
+   ee="what"
+
+
+
+
+
+
+
+
+print('''
+             
+
+
+                                      ,==.              |~~~       全部抓取完成
+                                     /  66\             |
+                                     \c  -_)         |~~~        '''+"本次一共抓取了 "+str(aaa)+" 个问题"+'''
+                                      `) (           |
+                                      /   \       |~~~           '''+"本次一共抓取了 "+str(bbb)+" 个答案"+'''
+                                     /   \ \      |
+                                    ((   /\ \_ |~~~              '''+"本次一共用了  "+str(mytime/3600).split('.')[0]+" 个小时"+'''
+                                     ||  \ `--`|
+                                     / / /  |~~~
+                                ___ (_(___)_|  
+
+
+
+    ''')
+
+
+
+
 
 
 
